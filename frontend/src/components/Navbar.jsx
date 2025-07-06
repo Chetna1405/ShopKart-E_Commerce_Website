623b54620ed9089eff9158217f0d4dcb2a636198
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-07-07 00:25:45 +0530

    Add search bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,66 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import shopkart from "../assets/shopkart.png"
 import { IoSearchCircleOutline } from "react-icons/io5";
 import { FaCircleUser } from "react-icons/fa6";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoSearchCircleSharp } from "react-icons/io5";
+
 import { UserDataContext } from '../context/UserDataContext';
 
 
 function Navbar() {
         let { userdata } = useContext(UserDataContext);
+        let [showSearch, setShowSearch] = useState(false);
 
         return (
-        <div className='w-[100vw] h-[70px] bg-[#ecfafaec] z-10 fixed top-0 flex items-center
+                <div className='w-[100vw] h-[70px] bg-[#ecfafaec] z-10 fixed top-0 flex items-center
                 justify-between px-[30px] shadow-md shadow-black'>
-                <div className='w-[30%] flex items-center justify-start gap-[10px] cursor-pointer'>
-                        <img src={shopkart} alt="" className='w-[30px] ' />
-                        <h1 className='text-[25px] text-[black] font-sans '>Shopkart</h1>
-                </div>
-
-                <div className='w-[40%] '>
-                        <ul className='flex items-center justify-center gap-[19px] text-white '>
-                                <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
-                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>HOME</li>
-                                <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
-                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>COLLECTIONS</li>
-                                <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
-                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>ABOUT</li>
-                                <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
-                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>CONTACT</li>
-                        </ul>
-                </div>
-
-                <div className='w-[30%] flex items-center justify-end gap-[20px] '>
-                <IoSearchCircleOutline className='w-[38px] h-[38px] text-[#000000] cursor-pointer ' />
-                {!userdata && <FaCircleUser className='w-[29px] h-[29px] text-[#000000] cursor-pointer ' />}
-                {
-                        userdata && 
-                        <div className='w-[30px] h-[30px] bg-[#000000] text-[white] rounded-full flex items-center justify-center ' >
-                                {userdata?.name.toUpperCase().slice(0,1)}
+                        <div className='w-[30%] flex items-center justify-start gap-[10px] cursor-pointer'>
+                                <img src={shopkart} alt="" className='w-[30px] ' />
+                                <h1 className='text-[25px] text-[black] font-sans '>Shopkart</h1>
                         </div>
-                }
-                <MdOutlineShoppingCart className='w-[30px] h-[30px] text-[#000000] cursor-pointer ' />
-                <p className='absolute w-[18px] h-[18px] items-center justify-center bg-black px-[5px] py-[2px] 
+
+                        <div className='w-[40%] '>
+                                <ul className='flex items-center justify-center gap-[19px] text-white '>
+                                        <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
+                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>HOME</li>
+                                        <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
+                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>COLLECTIONS</li>
+                                        <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
+                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>ABOUT</li>
+                                        <li className='text-[15px] hover:bg-slate-500 cursor-pointer 
+                                bg-[#000000c9] py-[10px] px-[20px] rounded-2xl '>CONTACT</li>
+                                </ul>
+                        </div>
+
+                        <div className='w-[30%] flex items-center justify-end gap-[20px] '>
+                                {!showSearch && <IoSearchCircleOutline
+                                        className='w-[38px] h-[38px] text-[#000000] cursor-pointer '
+                                        onClick={() => setShowSearch(prev => !prev)}
+                                />}
+                                {showSearch && <IoSearchCircleSharp
+                                        className='w-[39px] h-[39px] text-[#000000] cursor-pointer '
+                                        onClick={() => setShowSearch(prev => !prev)}
+                                />}
+
+                                {!userdata && <FaCircleUser className='w-[29px] h-[29px] text-[#000000] cursor-pointer ' />}
+                                {
+                                        userdata &&
+                                        <div className='w-[30px] h-[30px] bg-[#000000] text-[white] rounded-full flex items-center justify-center ' >
+                                                {userdata?.name.toUpperCase().slice(0, 1)}
+                                        </div>
+                                }
+                                <MdOutlineShoppingCart className='w-[30px] h-[30px] text-[#000000] cursor-pointer ' />
+                                <p className='absolute w-[18px] h-[18px] items-center justify-center bg-black px-[5px] py-[2px] 
                                 text-white rounded-full text-[9px] top-[10px] right-[23px] '>10</p>
-        </div>
-        </div>
+                        </div>
+
+                        {showSearch && <div className='w-[100%] h-[80px] bg-[#d8f6f9dd] absolute top-[100%] left-0 right-0 flex items-center justify-center '>
+                                <input type="text" className='w-[50%] h-[60%] bg-[#233533] rounded-[30px] px-[50px] placeholder:text-white text-[white] text-[18px] '
+                                        placeholder='Seacrch here ...'
+                                />
+                        </div>}
+                </div>
         )
 }
 
